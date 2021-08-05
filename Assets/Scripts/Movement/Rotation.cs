7ab99b0929ec45e6b4b5fb49cadd9c7169b47a18
Retekus <wegaseee@yandex.ru>
2021-08-05 14:09:15 +0300

    Update Rotation.cs

--- Assets/Scripts/Movement/Rotation.cs
+++ Assets/Scripts/Movement/Rotation.cs
@@ -19,41 +19,7 @@ public class Rotation : MonoBehaviour
     public void Look()
     {
 
-        //
-        /*
-        Vector3 currentPos = new Vector3(_rigidbody.position.x, 0f, _rigidbody.position.z);
-        Vector3 pointToLook = GetPointToLook();
-        Vector3 targetDirection = pointToLook - currentPos;
-
-        var forward = transform.forward;
-        var localTarget = transform.InverseTransformPoint(pointToLook);
-
-        Angle = Mathf.Atan2(localTarget.x, localTarget.z) * Mathf.Rad2Deg;
-
-        Vector3 eulerAngleVelocity = new Vector3(0, Angle, 0);
-        Quaternion deltaRotation = Quaternion.Euler(eulerAngleVelocity * Time.deltaTime);
-        _rigidbody.MoveRotation(_rigidbody.rotation * deltaRotation);
-        
-
-        
-        Vector3 currentPos = new Vector3(_rigidbody.position.x, 0f, _rigidbody.position.z);
-        Vector3 pointToLook = GetPointToLook();
-
-        Vector3 currentPosForward = currentPos +  Vector3.forward;
-        Vector3 pointToLookNormalized = (currentPos + pointToLook).normalized;
-
-        float angleBetween = Angle = Vector3.SignedAngle(currentPosForward, pointToLookNormalized, currentPos + Vector3.up);
-        //float angleBetween = Angle = Vector3.SignedAngle(currentPosForward, pointToLookNormalized, currentPos * Vector3.up);
-
-        Quaternion angleToRotate = Quaternion.AngleAxis(angleBetween, currentPos + Vector3.up);
-        //_rotationAngle = Quaternion.RotateTowards(_rotationAngle, angleToRotate, 180f);
-            
-        _rigidbody.MoveRotation(angleToRotate);
-
-        Debug.DrawLine(currentPos, pointToLookNormalized * 2, Color.blue);
-        Debug.DrawLine(currentPos, currentPos + Vector3.up * 2, Color.magenta);
-        Debug.DrawLine(currentPosForward * 2, pointToLook, Color.green);
-        Debug.DrawLine(currentPos, currentPosForward * 2, Color.cyan); */
+        //test
     }  
 
     public Vector3 GetPointToLook()
